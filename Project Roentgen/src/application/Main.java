101d7974ed598f2569c85ebc3bc2854f871b7edc
Joshua Knight <101josh10@gmail.com>
2018-03-09 14:49:43 -0500

    Set up so that when run, The program loads the Radiologist View

--- Project Roentgen/src/application/Main.java
+++ Project Roentgen/src/application/Main.java
@@ -1,20 +1,30 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			/*TabPane root = new TabPane();
+			Scene scene = new Scene(root,972,538);
+			scene.getStylesheets().add(getClass().getResource("../view/RadiologistView.fxml").toExternalForm());
+			stage.setTitle("RIS Radiologist View");
+			stage.setScene(scene);
+			stage.show();*/
+			
+			Parent root = FXMLLoader.load(getClass().getResource("../view/RadiologistView.fxml"));
+			Scene scene = new Scene(root, 972, 538);
+			stage.setTitle("RIS Radiologist View");
+			stage.setScene(scene);
+			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
