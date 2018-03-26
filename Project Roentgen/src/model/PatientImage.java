33cd1eaa561717afb2f77f93f288fab0b54f43e3
Joshua Knight <101josh10@gmail.com>
2018-03-26 14:43:25 -0400

    Created PatientImage class

---
+++ Project Roentgen/src/model/PatientImage.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Calendar;
+
+import javafx.scene.image.Image;
+
+public class PatientImage {
+	private Image rawImage;
+	private Image thumbnail;
+	private Calendar dateAdded;
+	private String description;
+	
+	public PatientImage(){
+		
+	}
+}
