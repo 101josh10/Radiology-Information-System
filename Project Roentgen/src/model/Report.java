d28641fc65e58a3ebbe5a4b5ebebfa96c650fbcc
Joshua Knight <101josh10@gmail.com>
2018-03-26 14:44:25 -0400

    Update

--- Project Roentgen/src/model/Report.java
+++ Project Roentgen/src/model/Report.java
@@ -9,10 +9,40 @@ public class Report {
 	private File reportFile;
 	private String title;
 	private Calendar dateAdded;
-	private String fileType;
 	private Button downloadButton;
 
 	public Report(){
-
+		downloadButton = new Button();
+		downloadButton.setText("Download");
 	}
+
+	public File getReportFile() {
+		return reportFile;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Calendar getDateAdded() {
+		return dateAdded;
+	}
+
+	public Button getDownloadButton() {
+		return downloadButton;
+	}
+
+	public void setReportFile(File reportFile) {
+		this.reportFile = reportFile;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setDateAdded(Calendar dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+	
+	
 }
