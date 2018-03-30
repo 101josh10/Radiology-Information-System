824ba3db0c53e5a1a3c67b60b5aeaf152f5d5528
Joshua Knight <101josh10@gmail.com>
2018-03-30 14:17:36 -0400

    Added database info

--- Project Roentgen/src/model/DBConnection.java
+++ Project Roentgen/src/model/DBConnection.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 import application.Main;
 
 public class DBConnection implements Runnable {
-	public static final String DBPASSWORD = "admin";
+	public static final String DBPASSWORD = "Chrome777";
 	Connection Con1;
 	
 	public void run(){
 		System.out.println("Establishing connection to database");
 		
 		try {
-			Con1 = DriverManager.getConnection("jdbc:mysql://164.132.49.5:3306","admin", DBPASSWORD);
+			Con1 = DriverManager.getConnection("jdbc:mysql://50.62.209.15:3306","wesmdonald", DBPASSWORD);
 			System.out.println("Connection success!");
 			
 			Main.con = Con1;
