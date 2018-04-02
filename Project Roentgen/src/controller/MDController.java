c94b21627d8906c43f00f8cd71206388b3f252ca
bendenton <bendenton@168.30.57.68>
2018-04-02 15:03:51 -0400

    Set things up in

--- Project Roentgen/src/controller/MDController.java
+++ Project Roentgen/src/controller/MDController.java
@@ -1,7 +1,26 @@
 package controller;
-import ;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.MediaView;
 
 public class MDController 
 {
 	
+	@FXML private ListView mdListView = new ListView();
+	@FXML private AnchorPane images = new AnchorPane();
+	@FXML private ListView mdPatientInfoListView = new ListView();
+	@FXML private MediaView radiologistReportMediaView = new MediaView();
+	@FXML private ListView patientRequestListView = new ListView();
+	@FXML private TextField patientId = new TextField();
+	@FXML private TextField patientFirstName = new TextField();
+	@FXML private TextField patientLastName = new TextField();
+	@FXML private TextField requestBodyPart = new TextField();
+	@FXML private TextArea requestDescription = new TextArea();
+	@FXML private ChoiceBox procedureType = new ChoiceBox();
+	@FXML private Button submitRequest = new Button();
 }
