12719b78c19f51958497eb4f5d4f205537c52314
Joshua Knight <101josh10@gmail.com>
2018-04-02 14:48:01 -0400

    added variables to match the view objects

--- Project Roentgen/src/controller/ReceptionistController.java
+++ Project Roentgen/src/controller/ReceptionistController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -23,8 +26,29 @@ public class ReceptionistController {
 	@FXML private ChoiceBox modalityChoiceBox;
 	@FXML private TextField bodyPartsTextField;
 	@FXML private TextArea additionalTextArea;
-	
+
 	//Appointment Time Slot TableView
 	@FXML private TableView<Appointment> appointmentTableView;
-	
+	@FXML private TableColumn<Appointment, String> timeTableColumn;
+	@FXML private TableColumn<Appointment, String> xrayTableColumn;
+	@FXML private TableColumn<Appointment, String> ctTableColumn;
+	@FXML private TableColumn<Appointment, String> ultrasoundTableColumn;
+	@FXML private TableColumn<Appointment, String> mriTableColumn;
+
+	//Appointment Info
+	@FXML private DatePicker apptInfoDatePicker;
+	@FXML private ChoiceBox apptInfoTimeChoiceBox;
+	@FXML private ChoiceBox apptInfoPatientChoiceBox;
+	@FXML private ChoiceBox apptInfoModalityChoiceBox;
+	@FXML private TextField apptInfoBodyPartsTextField;
+	@FXML private TextField apptInfoDescTextArea;
+
+	//Other Variables
+	ObservableList<String> timeSlotChoices = FXCollections.observableArrayList();
+	ObservableList<String> patientChoices = FXCollections.observableArrayList();
+	ObservableList<String> modalityChoices = FXCollections.observableArrayList();
+
+	public void initialize(){
+
+	}
 }
