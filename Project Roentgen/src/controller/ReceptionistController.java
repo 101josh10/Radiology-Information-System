ea75284a5e670d2db3e6c6d6bce4a98510c5b277
Joshua Knight <101josh10@gmail.com>
2018-04-03 00:35:32 -0400

    Started filling in ObservableLists

--- Project Roentgen/src/controller/ReceptionistController.java
+++ Project Roentgen/src/controller/ReceptionistController.java
@@ -41,7 +41,7 @@ public class ReceptionistController {
 	@FXML private ChoiceBox apptInfoPatientChoiceBox;
 	@FXML private ChoiceBox apptInfoModalityChoiceBox;
 	@FXML private TextField apptInfoBodyPartsTextField;
-	@FXML private TextField apptInfoDescTextArea;
+	@FXML private TextArea apptInfoDescTextArea;
 
 	//Other Variables
 	ObservableList<String> timeSlotChoices = FXCollections.observableArrayList();
@@ -50,5 +50,54 @@ public class ReceptionistController {
 
 	public void initialize(){
 
+		initTimeSlots();
+	}
+	
+	public void initTimeSlots() {
+		String time;
+		for(int hours = 8; hours < 17; hours++) {
+			time = "";
+			for(int mins = 0; mins < 60; mins += 15) {
+				if(hours < 12) {
+					if(mins == 0){
+						time = hours + ":" + mins + "0 AM";
+					}
+					else {
+						time = hours + ":" + mins + " AM";
+					}
+				}
+				else if(hours > 12) {
+					int formatted = hours % 12;
+					if(mins == 0){
+						time = hours + ":" + mins + "0 PM";
+					}
+					else {
+						time = hours + ":" + mins + " PM";
+					}
+				}
+				System.out.println(time);
+				timeSlotChoices.add(time);
+			}
+		}
+	}
+	
+	public void updateTimeSlots() {
+		
+	}
+	
+	public void clearFiltersPressed() {
+		
+	}
+	
+	public void createAppointmentPushed() {
+		
+	}
+	
+	public void editAppointmentPushed() {
+		
+	}
+	
+	public void deleteAppointmentPushed() {
+		
 	}
 }
