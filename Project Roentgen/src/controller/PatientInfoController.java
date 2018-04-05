149065e7d94228a2b1c4b31068ba30c657bb356a
Joshua Knight <101josh10@gmail.com>
2018-04-04 23:22:29 -0400

    Class created

--- Project Roentgen/src/controller/PatientInfoController.java
+++ Project Roentgen/src/controller/PatientInfoController.java
@@ -1,5 +1,13 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class PatientInfoController {
 
+	@FXML private TextField firstNameTextField;
+	
+	public void initialize() {
+		
+	}
 }
