6c16b6ccde6589615e2dc90e90db6ce9233b7f87
Joshua Knight <101josh10@gmail.com>
2018-04-04 23:30:40 -0400

    added onAction functions

--- Project Roentgen/src/controller/PatientInfoController.java
+++ Project Roentgen/src/controller/PatientInfoController.java
@@ -1,13 +1,46 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class PatientInfoController {
 
 	@FXML private TextField firstNameTextField;
+	@FXML private TextField lastNameTextField;
+	@FXML private TextField middleInitialTextField;
+	@FXML private ComboBox<String> genderComboBox;
+	@FXML private ComboBox<String> ethnicityComboBox;
+	@FXML private TextField otherEthnicityTextField;
+	@FXML private TextField ssnTextField;
+	@FXML private DatePicker dobDatePicker;
+	@FXML private TextField phoneNumTextField;
+	@FXML private TextField emailTextField;
+	@FXML private TextField addressTextField;
+	@FXML private TextField stateTextField;
+	@FXML private TextField cityTextField;
+	@FXML private TextField zipTextField;
+	@FXML private Button editButton;
 	
 	public void initialize() {
+		
+	}
+	
+	public void middleInitialKeyTyped() {
+		
+	}
+	
+	public void ethnicitySelected() {
+		
+	}
+	
+	public void editButtonPressed() {
+		
+	}
+	
+	public void deleteButtonPressed() {
 		
 	}
 }
