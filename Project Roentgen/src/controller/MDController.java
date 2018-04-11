6983305419a11e38dfc8cffce452d834e4b68a71
bendenton <bendenton@168.30.58.56>
2018-04-11 14:52:10 -0400

    Did changes

--- Project Roentgen/src/controller/MDController.java
+++ Project Roentgen/src/controller/MDController.java
@@ -1,21 +1,30 @@
 package controller;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.MediaView;
+import model.Patient;
+import model.PatientImage;
+import model.Report;
 
 public class MDController 
 {
-	
-	@FXML private ListView mdListView = new ListView();
-	@FXML private AnchorPane images = new AnchorPane();
-	@FXML private ListView mdPatientInfoListView = new ListView();
-	@FXML private MediaView radiologistReportMediaView = new MediaView();
-	@FXML private ListView patientRequestListView = new ListView();
+	//Set up the Main M.D. Tab Fields
+	@FXML private ListView<Patient> mdListView = new ListView();
+	@FXML private ImageView images = new ImageView();
+	@FXML private ListView<Patient> mdPatientInfoListView = new ListView();
+	@FXML private ImageView radiologistReportImageView = new ImageView();
+	@FXML private ListView<Patient> patientRequestListView = new ListView();
+	//Set up the M.D. Request Tab Fields
 	@FXML private TextField patientId = new TextField();
 	@FXML private TextField patientFirstName = new TextField();
 	@FXML private TextField patientLastName = new TextField();
@@ -23,4 +32,24 @@ public class MDController
 	@FXML private TextArea requestDescription = new TextArea();
 	@FXML private ChoiceBox procedureType = new ChoiceBox();
 	@FXML private Button submitRequest = new Button();
+	//Other needed variables
+	private ObservableList<Patient> patientList = FXCollections.observableArrayList();
+	private ObservableList<Report> patientReports = FXCollections.observableArrayList();
+	private ObservableList<PatientImage> patientImages = FXCollections.observableArrayList();
+	private Patient selectedPatient;
+	
+	//M.D. View Current Patients Click
+	mdListview.@setOnItemClickListener(new OnItemClickListener() 
+	{
+		public void OnItemClick()
+		{
+			//images set
+			images.
+			//patient Info set
+			//Report set
+		}
+	});
+	
+	//Database Stuff
+	
 }
