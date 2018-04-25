09abf0c6003bddc077222fec3fad6e65c1189fc1
Joshua Knight <101josh10@gmail.com>
2018-04-24 22:33:34 -0400

    Added Getters and Setters

--- Project Roentgen/src/model/PatientImage.java
+++ Project Roentgen/src/model/PatientImage.java
@@ -1,16 +1,48 @@
 package model;
 
-import java.util.Calendar;
+import java.time.LocalDate;
 
 import javafx.scene.image.Image;
 
 public class PatientImage {
 	private Image rawImage;
 	private Image thumbnail;
-	private Calendar dateAdded;
+	private LocalDate dateAdded;
 	private String description;
 	
 	public PatientImage(){
 		
+	}
+	
+	public Image getRawImage() {
+		return rawImage;
+	}
+	
+	public Image getThumbnail() {
+		return thumbnail;
+	}
+	
+	public LocalDate getDateAdded() {
+		return dateAdded;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setRawImage(Image i) {
+		this.rawImage = i;
+	}
+	
+	public void setThumbnail(Image i) {
+		this.thumbnail = i;
+	}
+	
+	public void setDateAdded(LocalDate ld) {
+		this.dateAdded = ld;
+	}
+	
+	public void setDescription(String desc) {
+		this.description = desc;
 	}
 }
